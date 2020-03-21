f0ed495779a17289e4d8bef7209a149a21908bb6
Julia Zavorokhina <jzavorohina@gmail.com>
2020-03-21 22:17:55 +0300

    feat: add file script.js

---
+++ script.js
@@ -0,0 +1,69 @@
+// To be continued
+
+  function navClickHandler() {
+
+  }
+ 
+  function onChangeScroll() {
+
+  }
+  
+  function isPageEnd() {
+
+  }
+  
+  function changeActiveNav() {
+   
+  }
+  
+  function onChangeHeader() {
+
+  }
+
+  function scrollTopHandler() {
+ 
+  }
+
+  function tagClickHandler() {
+
+  }
+  
+  function shufflePortfolio() {
+
+  }
+ 
+  function portfolioClickHandler() {
+
+  }
+  
+  function togglePhone() {
+
+  }
+
+  function changeSlideLeft() {
+ 
+  }
+  
+  function changeSlideRight() {
+
+  }
+  
+  function changeSlide() {
+
+  }
+  
+  function changeSlideClass() {
+ 
+  }
+
+  function onSubmitForm() {
+
+  }
+
+  function createModal() {
+
+  }
+  
+  function singoloModal() {
+
+  }
